684e9518a04e3ea94597ed03c4620da6814623ab
= <prithvi2410rj@gmail.com>
2022-05-06 16:39:54 +0530

    added config in program cs

---
+++ Models/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMS.API.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public int StatusId { get; set; }
+        [NotMapped]
+        public int TrainerId { get; set; }
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public string Description { get; set; }
+
+        public CourseStatus? Status { get; set; }
+        [NotMapped]
+        public virtual User? Trainer { get; set; }
+        public virtual Department? Department { get; set; }
+        public virtual List<Topic>? Topics { get; set; }
+        public virtual List<User>? Trainees { get; set; }
+        public virtual List<CourseFeedback>? Feedbacks { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public int? UpdatedBy { get; set; }
+    }
+}
