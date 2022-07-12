b169c5f340fc15e0eb7c1589176e3371d6f48207
Gokul Ravi <gokul.ravi@aspiresys.com>
2022-07-12 08:39:43 +0530

    code cleanup

--- TMS.BAL/Models/User.cs
+++ TMS.BAL/Models/User.cs
@@ -6,9 +6,7 @@ namespace TMS.BAL
     {
         public int Id { get; set; }
         [Required]
-        //[Range(1, 5, ErrorMessage = "Role Id Must be Between 1 to 5. No other Role Id's are supported")]
         public int RoleId { get; set; }
-        //[Range(1, 3, ErrorMessage = "Department Id Must be Between 1 to 3. No other Department Id's are supported")]
         public int? DepartmentId { get; set; }
         [Required]
         public string FullName { get; set; }
