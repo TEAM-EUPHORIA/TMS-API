80cde30a8be9ef0e450bb11c4a9ef1f34153e0f7
876gokul <876gokul@gmail.com>
2022-07-20 17:42:37 +0530

    added status to topic

--- TMS.BAL/Models/Topic.cs
+++ TMS.BAL/Models/Topic.cs
@@ -3,18 +3,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TMS.BAL
 {
-    public class Topic:AuditFields
+    public class Topic : AuditFields
     {
         [Key]
         public int TopicId { get; set; }
         [Required]
         public int CourseId { get; set; }
         [Required]
-        public string ?Name { get; set; }
+        public string? Name { get; set; }
         [Required]
-        public string ?Duration { get; set; }
+        public string? Duration { get; set; }
         [Required]
-        public string ?Content { get; set; }
+        public string? Content { get; set; }
+        public bool Status { get; set; }
         public bool? isDisabled { get; set; }
         // virtual navigation properties
         public virtual Course? Course { get; set; }
